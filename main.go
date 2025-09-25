1d3663fce304b952f96081d8a9cf698286e4207e
evanwiseman <evanwiseman2000@gmail.com>
2025-09-24 21:14:31 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	server := http.Server{
+		Handler: serveMux,
+		Addr:    ":8080",
+	}
+	server.ListenAndServe()
+}
