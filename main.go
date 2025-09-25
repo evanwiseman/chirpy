7cfdd432bf0f5915caa402fc98c2a9ef7616baef
evanwiseman <evanwiseman2000@gmail.com>
2025-09-25 11:20:22 -0700

    added file server and metrics

--- main.go
+++ main.go
@@ -1,15 +1,73 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+const (
+	serverPort     = "8080"
+	fileServerPath = "."
+)
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "OK")
+}
+
+func (cfg *apiConfig) Handler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileServerHits.Load()
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %v", hits)
+}
+
+func (cfg *apiConfig) Reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(0)
+	hits := cfg.fileServerHits.Load()
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Reset Metrics. Hits: %v", hits)
+}
+
 func main() {
+	apiCfg := apiConfig{}
+
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
+	// Create file server handlers
+	fileServerHandler := http.FileServer((http.Dir(fileServerPath)))
+	appHandler := http.StripPrefix("/app", fileServerHandler)
+
+	// Attach handlers to the serve mux
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(appHandler))
+	serveMux.HandleFunc("/healthz", healthzHandler)
+	serveMux.HandleFunc("/metrics", apiCfg.Handler)
+	serveMux.HandleFunc("/reset", apiCfg.Reset)
+
+	// Create the server at the desired port and attach the serve mux
 	server := http.Server{
 		Handler: serveMux,
-		Addr:    ":8080",
+		Addr:    ":" + serverPort,
 	}
-	server.ListenAndServe()
+
+	// Start the server
+	log.Printf("Serving files from %s on port: %s\n", fileServerPath, serverPort)
+	log.Fatal(server.ListenAndServe())
 }
