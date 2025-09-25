4b82847f51ac91616e1d1ca15ddeb7edee050805
evanwiseman <evanwiseman2000@gmail.com>
2025-09-25 14:37:24 -0700

    add validate chirps

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -29,7 +30,7 @@ func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "OK")
 }
 
-func (cfg *apiConfig) Handler(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
 	hits := cfg.fileServerHits.Load()
 
 	html := fmt.Sprintf(`<html>
@@ -46,13 +47,57 @@ func (cfg *apiConfig) Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, html)
 }
 
-func (cfg *apiConfig) Reset(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) metricsReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileServerHits.Store(0)
 	hits := cfg.fileServerHits.Load()
 
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Reset Metrics. Hits: %v", hits)
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	type returnVals struct {
+		Error string `json:"error"`
+		Valid bool   `json:"valid"`
+	}
+
+	// Set the header
+	w.Header().Set("Content-Type", "application/json")
+
+	// Prepare the response & status code
+	respBody := returnVals{}
+	status := http.StatusOK
+
+	// Decode the json from the request into Body
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respBody.Error = "Invalid JSON"
+		status = http.StatusBadRequest
+	} else if len(params.Body) > 140 {
+		respBody.Error = "Chirp is too long"
+		status = http.StatusBadRequest
+	} else {
+		respBody.Valid = true
+	}
+
+	data, err := json.Marshal(respBody)
+	if err != nil {
+		log.Printf("JSON marshal failed: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, `{"error":"internal server error"}`)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(data)
 }
 
 func main() {
@@ -67,8 +112,9 @@ func main() {
 	// Attach handlers to the serve mux
 	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(appHandler))
 	serveMux.HandleFunc("GET /api/healthz", healthzHandler)
-	serveMux.HandleFunc("GET /admin/metrics", apiCfg.Handler)
-	serveMux.HandleFunc("POST /admin/reset", apiCfg.Reset)
+	serveMux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	serveMux.HandleFunc("POST /admin/reset", apiCfg.metricsReset)
+	serveMux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 	// Create the server at the desired port and attach the serve mux
 	server := http.Server{
